74f5e55cbbab1db6991213c6d07a5a786d602851
Jason Lind <jason.lind@groupbobaloo.com>
2018-01-18 19:40:04 -0600

    inital checkin

---
+++ run.Transformation.Xaml/StringApiBinding.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+using System.Net.Http;
+using System.Windows.Data;
+
+namespace run.Transformation.Xaml
+{
+    [MarkupExtensionReturnType(typeof(string))]
+    public class StringApiBinding : MarkupExtension
+    {
+        [ConstructorArgument("Endpoint")]
+        public Uri Endpoint { get; set; }
+        public StringApiBinding(Uri endopoint) { Endpoint = endopoint; }
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            HttpClient client = new HttpClient();
+            var response = client.GetAsync(Endpoint);
+            response.Wait();
+            response.Result.EnsureSuccessStatusCode();
+            var content = response.Result.Content.ReadAsStringAsync();
+            content.Wait();
+            return content.Result;
+        }
+        
+    }
+}
