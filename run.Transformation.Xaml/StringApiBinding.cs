bcbc482b60dd7c6529d368bbb3b1490d9dee03f2
Jason Lind <jason.lind@groupbobaloo.com>
2018-01-19 18:58:35 -0600

    closer

--- run.Transformation.Xaml/StringApiBinding.cs
+++ run.Transformation.Xaml/StringApiBinding.cs
@@ -6,19 +6,33 @@ using System.Threading.Tasks;
 using System.Windows.Markup;
 using System.Net.Http;
 using System.Windows.Data;
+using System.Windows;
+using System.Windows.Controls;
+using System.ComponentModel;
 
 namespace run.Transformation.Xaml
 {
+    public class UriDependencyObject : DependencyObject
+    {
+        public string Uri
+        {
+            get { return GetValue(EndpointProperty) as string; }
+            set { SetCurrentValue(EndpointProperty, value); }
+        }
+        public static readonly DependencyProperty EndpointProperty = DependencyProperty.Register(nameof(Uri), typeof(string), typeof(UriDependencyObject));
+    }
     [MarkupExtensionReturnType(typeof(string))]
     public class StringApiBinding : MarkupExtension
     {
-        [ConstructorArgument("endpoint")]
-        public string Endpoint { get; set; }
-        public StringApiBinding(string endopoint) { Endpoint = endopoint; }
+        public BindingBase Endpoint { get; set; }
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
+            UriDependencyObject obj = new UriDependencyObject();
+            BindingOperations.SetBinding(obj, UriDependencyObject.EndpointProperty, Endpoint);
+            
+            var uri = obj.GetValue(UriDependencyObject.EndpointProperty) as string;
             HttpClient client = new HttpClient();
-            var response = client.GetAsync(Endpoint);
+            var response = client.GetAsync(uri);
             response.Wait();
             response.Result.EnsureSuccessStatusCode();
             var content = response.Result.Content.ReadAsStringAsync();
